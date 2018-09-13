dab25a36a6f08df5a5198f268cb848d33b9d2407
hermask8 <hermask8@gmail.com>
2018-09-13 10:03:25 -0600

    Vista arbol

---
+++ app/src/main/java/com/example/ervi/lab1_compresion/Arbol.java
@@ -0,0 +1,16 @@
+package com.example.ervi.lab1_compresion;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class Arbol {
+    List<Nodo> nodos = new ArrayList<>();
+    Hashtable<String,String> valorCracter = new Hashtable<>();
+
+    public void Agregar()
+    {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/ervi/lab1_compresion/ComprimirVista.java
@@ -0,0 +1,13 @@
+package com.example.ervi.lab1_compresion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComprimirVista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comprimir_vista);
+    }
+}
