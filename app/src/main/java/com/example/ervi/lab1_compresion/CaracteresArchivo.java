82d574e0634438ac8c2f39a18b15afea8b2d1eb1
hermask8 <hermask8@gmail.com>
2018-09-18 18:25:25 -0600

    Agregar datos a un array

--- app/src/main/java/com/example/ervi/lab1_compresion/CaracteresArchivo.java
+++ app/src/main/java/com/example/ervi/lab1_compresion/CaracteresArchivo.java
@@ -1,27 +1,35 @@
 package com.example.ervi.lab1_compresion;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CaracteresArchivo {
 
-    private Hashtable<Character,Integer> caracteres = new Hashtable<>();
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    private List<Caracter> caracteres = new ArrayList<>();
+
     private void SepararLinea(String texto)
     {
-        for(int i=0 ;i <texto.length();i++)
+        for (int i=0;i<texto.length();i++)
         {
-            char nuevo = texto.charAt(i);
-            if (caracteres.containsValue(nuevo)==true)
+            char letra = texto.charAt(i);
+            Caracter miCaracter = new Caracter();
+            miCaracter.setValorCaracter(letra);
+            miCaracter.setConteo(1);
+            boolean contiene = true;
+            for (Caracter dato:caracteres)
             {
-
-                caracteres.replace(nuevo,caracteres.get(nuevo).intValue(),(caracteres.get(nuevo).intValue()+1));
+                if (dato.getValorCaracter().equals(miCaracter)==true)
+                {
+                    int conteo = dato.getConteo() + 1 ;
+                    dato.setConteo(conteo);
+                }
+                contiene = false;
             }
-            else
+            if (contiene==false)
             {
-                caracteres.put(nuevo,1);
+                caracteres.add(miCaracter);
+                contiene=true;
             }
         }
     }
