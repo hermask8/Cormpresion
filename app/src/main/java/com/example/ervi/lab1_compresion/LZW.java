856e575fa3d0ae33daaf3d01842108e8e156477d
hermask8 <hermask8@gmail.com>
2018-09-25 22:03:14 -0600

    Implementacion de ventanas

--- app/src/main/java/com/example/ervi/lab1_compresion/LZW.java
+++ app/src/main/java/com/example/ervi/lab1_compresion/LZW.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pruebalzw;
+package com.example.ervi.lab1_compresion;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
--- app/src/main/java/com/example/ervi/lab1_compresion/LZWVentana.java
+++ app/src/main/java/com/example/ervi/lab1_compresion/LZWVentana.java
@@ -1,13 +1,122 @@
 package com.example.ervi.lab1_compresion;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LZWVentana extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_STORAGE = 1000;
+    private static final int READ_REQUEST_CODE = 42;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lzwventana);
+
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1000);
+        }
+
     }
+
+    private void performFileSearch()
+    {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent,READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode== READ_REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            if (data!=null)
+            {
+                Uri uri = data.getData();
+                String path = uri.getPath();
+                path = path.substring(path.indexOf(":") + 1);
+                Toast.makeText(this,"" + path,Toast.LENGTH_SHORT).show();
+
+
+            /*
+            ;
+             pathArchivo2 = path;
+                tvPath.setText(path);
+                pathArchivo3 =path.substring(0,path.indexOf("."));
+            tvBinario.setText(miArbol.getBinarioVentana());
+            tvAscii.setText(miArbol.getPasarAscii());
+            */
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==PERMISSION_REQUEST_STORAGE)
+        {
+            if (grantResults[0]== PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this,"permission granted!",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this,"Permission not granted",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    public void escribirArchivo()
+    {
+/*
+       // File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),fileName);
+        try {
+           // FileOutputStream fos2 = new FileOutputStream(file);
+
+         //   String guardar2 = textoDescomprimido;
+            //fos2.write(guardar2.getBytes());
+            //fos2.close();
+            Toast.makeText(this,"Guardado",Toast.LENGTH_SHORT).show();
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this,"Archivo no encontrado",Toast.LENGTH_SHORT).show();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this,"ERROR Guardando",Toast.LENGTH_SHORT).show();
+        }
+*/
+
+
+    }
+
+
+
 }
